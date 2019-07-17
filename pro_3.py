c521428b72eabc8f39e7bf532d9e1fc682fef483
Megha Sharma G <megha.sharma3333@gmail.com>
2019-07-17 09:57:55 +0530

    Update pro_3.py

--- pro_3.py
+++ pro_3.py
@@ -2,6 +2,8 @@ a,b=input().split()
 l=abs(len(a)-len(b))
 def conv(pro,x,y):
     for i in range(len(a)):
+        if len(y)==1  and y[i]==x[i]:
+            break
         if x[i]!=y[i]:
             pro+=1
     return pro
